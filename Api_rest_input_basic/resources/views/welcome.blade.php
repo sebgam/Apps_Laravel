2bcb2ed05d204c4d39e867db5c1c19df6a34acd3
sebas <sebastiangamboa11@gmail.com>
2017-05-06 16:56:11 -0500

    welcome

--- Api_rest_input_basic/resources/views/welcome.blade.php
+++ Api_rest_input_basic/resources/views/welcome.blade.php
@@ -77,18 +77,7 @@
                 </div>
             @endif
 
-            <form action="{{url('subscribe')}}" method="POST">
-              <script
-                src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-                data-key="{{config('services.stripe.key')}}"
-                data-amount="1000"
-                data-name="Demo Site"
-                data-description="Widget"
-                data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
-                data-locale="auto"
-                data-currency="eur">
-              </script>
-            </form>
+           
         </div>
     </body>
 </html>
